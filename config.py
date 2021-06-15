982bd982301bad6eac99af40fae712afc00ff890
ds-praveenkumar <praveenkr2208@gmail.com>
2021-06-16 00:18:49 +0530

    add logger

--- config.py
+++ config.py
@@ -2,7 +2,13 @@
 
 from pathlib import Path
 
+class Config:
+    """ folder constants """"
+    #paths
+    root = Path.cwd().resolve()
+    data = Path(root) / "data"
+    notebook = Path( root) / "notebook"
+    model = Path( root ) / "model"
 
-root = Path.cwd().resolve()
-data = Path(root) / "data"
+    
 
